1f5b9899744abf46af650cc4728df301e81b9437
Md Moinuddin Mansoori <mdmoinuddinmansoori@gmail.com>
2023-10-16 18:03:13 +0530

    changed

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -23,18 +25,63 @@ def login():
         return "Invalid credentials. Please try again."
 
 
-@app.route('/signup', methods=['GET', 'POST'])
+
+@app.route('/signup')
 def signup_page():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-
-        # Add code to store user information in the database (dummy data in this case)
-
-        return redirect(url_for('login_page'))
-
     return render_template('signup.html')
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
+
+    # Check if username is unique (you might want to add email uniqueness as well)
+    if next((user for user in users if user['username'] == username), None):
+        return "Username already exists. Please choose a different one."
+
+    users.append({'username': username, 'email': email, 'password': password})
+    return redirect(url_for('login_page'))
+
+
+
+# Create a directory to store uploaded files if it doesn't exist
+if not os.path.exists('uploads'):
+    os.makedirs('uploads')
+
+@app.route('/upload_page')
+def upload_page():
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+
+    if file and allowed_file(file.filename):
+        filename = os.path.join('uploads', secure_filename(file.filename))
+        file.save(filename)
+
+        # Add code for data cleaning and preprocessing (using pandas)
+        df = pd.read_excel(filename)
+        
+        # Example data cleaning: Drop rows with missing values
+        df = df.dropna()
+
+        # Example data preprocessing: Add a new column
+        df['Total Sales'] = df['Quantity'] * df['Price']
+
+        # Save the cleaned and preprocessed data
+        cleaned_filename = os.path.join('uploads', 'cleaned_' + secure_filename(file.filename))
+        df.to_excel(cleaned_filename, index=False)
+
+        return redirect(url_for('dashboard'))
+    else:
+        return "Invalid file format. Please upload an Excel file."
+
+# Helper function to check if file extension is allowed
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'xlsx', 'xls'}
+
 
 
 @app.route('/dashboard')
